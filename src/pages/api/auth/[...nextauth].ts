def3e9451be0db4c013d044e539c42f836385b5f
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2022-12-06 11:34:43 +0800

    fix session & user type again

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,20 @@
-import NextAuth, { type Session, type NextAuthOptions } from "next-auth";
+import NextAuth, {
+  type Session,
+  type NextAuthOptions,
+  type User,
+} from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
-import { type User } from "../../../types/User.js";
+import { type AdapterUser } from "next-auth/adapters.js";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }: { session: Session; user: User }) {
+    session({ session, user }: { session: Session; user: User | AdapterUser }) {
       if (session.user) {
         session.user.id = user.id;
         session.user.role = user.role;
