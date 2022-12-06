24b3cb1141d4a8951a6cdb47710ada1d27edb591
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2022-12-06 12:02:13 +0800

    fix next auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,18 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
+import NextAuth, {
+  type Session,
+  type User,
+  type NextAuthOptions,
+} from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
-    session({ session, user }) {
+    session({ session, user }: { session: Session; user: User }) {
       if (session.user) {
         session.user.id = user.id;
         session.user.role = user.role;
